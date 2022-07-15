836f7687c0625e8a370783e894b0dd8136f79b31
Amran Haroon <haroon3rd@gmail.com>
2022-07-15 01:01:39 -0500

    Running version

---
+++ simplefortunecookie.py
@@ -0,0 +1,31 @@
+# import random
+# fortunes = ["Good things come to those who wait.",
+#             "Patience is a virtue.",
+#             "The early bird gets the worm.",
+#             "A wise man once said, everything in its own time and place.",
+#             "Fortune cookies rarely share fortunes.",
+#             "The fortune you seek is in another cookie.",
+#             "A foolish man listens to his heart. A wise man listens to cookies.",
+#             "A fanatic is one who can't change his mind, and won't change the subject.",
+#             "Flattery will go far tonight.",
+#             "You will live long enough to open many fortune cookies.",
+#             "A journey of a thousand miles begins with a single step.",
+#             "A new voyage begins with a new cookie.",
+#             "You will be successful in your first try.",
+#             "Some men dream of fortunes, others dream of cookies.",
+#             "Fortune not found? Abort, Retry, Ignore.",
+#             "Never forget a friend. Especially if he owes you.",
+#             "Someone will invite you to a Karaoke party.",
+#             "No snowflake feels responsible in an avalanche.",
+#             "A new idea can be your salvation in life.",
+#             "Fortune favors the brave.",
+#             "You are not illiterate.",
+#             "This is your lucky day.",
+#             "This is your day. Celebrate it."
+#             ]
+# print ("What is the bill amount? : ",end="")
+# bill = float(input())
+# random.seed(bill)
+# ran = random.randint(0,23)
+# print ("Your fortune is : \"" + fortunes[ran] + "\"")
+# #print (random.choice(fortunes))
